bb94db1c3c57fce299b403445742aee825fae014
Seonghyeon Cho <seonghyeoncho96@gmail.com>
2022-07-28 21:52:26 +0900

    Remove useless import

--- src/main/kotlin/com/choar/openmarket/domain/user/UserRepository.kt
+++ src/main/kotlin/com/choar/openmarket/domain/user/UserRepository.kt
@@ -1,6 +1,5 @@
 package com.choar.openmarket.domain.user
 
-import com.choar.openmarket.domain.user.User
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
