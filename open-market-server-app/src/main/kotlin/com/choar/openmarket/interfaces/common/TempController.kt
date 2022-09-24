baea030c14362ce324593b2272170adac441ea34
Seonghyeon Cho <seonghyeoncho96@gmail.com>
2022-09-24 23:24:32 +0900

    minor

--- open-market-server-app/src/main/kotlin/com/choar/openmarket/interfaces/common/TempController.kt
+++ open-market-server-app/src/main/kotlin/com/choar/openmarket/interfaces/common/TempController.kt
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/")
 class TempController {
+    // TODO: spring actuator
     @GetMapping("/")
     fun ok() = "ok"
 }
